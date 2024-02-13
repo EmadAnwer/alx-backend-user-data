c7d4727194361cd885c2c47335b5d1880e995081
EmadAnwer <emadanwer.official@gmail.com>
2024-02-14 00:56:14 +0200

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,10 @@
 """ Module of BasicAuth
 """
 
+from typing import TypeVar
 from .auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -42,3 +44,31 @@ class BasicAuth(Auth):
                     decoded_base64_authorization_header[index + 1:],
                 )
         return (None, None)
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar("User"):
+        """user_object_from_credentials"""
+        if user_email is None or user_pwd is None:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        try:
+            user = User.search({"email": user_email})
+            if user:
+                if user[0].is_valid_password(user_pwd):
+                    return user[0]
+            else:
+                return None
+        except Exception:
+            return None
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """current_user"""
+        auth_header = self.authorization_header(request)
+        b64_auth_header = self.extract_base64_authorization_header(auth_header)
+        decoded_auth_header = self.decode_base64_authorization_header(
+                                                b64_auth_header)
+        user, pwd = self.extract_user_credentials(decoded_auth_header)
+        return self.user_object_from_credentials(user, pwd)
