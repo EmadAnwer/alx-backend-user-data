7a501ddd8d2881f2d8823cb4e51d8bda21e77267
EmadAnwer <emadanwer.official@gmail.com>
2024-02-16 21:40:00 +0200

    [task 5]

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,12 +35,16 @@ def before_request():
             "/api/v1/status/",
             "/api/v1/unauthorized/",
             "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/"
         ]
         if auth.require_auth(request.path, excluded_paths):
             if not auth.authorization_header(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
+            if not bool(auth.authorization_header(request) and
+                        auth.session_cookie(request)):
+                abort(401)
 
         request.current_user = auth.current_user(request)
 
