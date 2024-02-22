3eead480fe1a5bfdf66d71a4dd3ee3c4951aa1af
EmadAnwer <emadanwer.official@gmail.com>
2024-02-22 23:20:13 +0200

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,16 +4,32 @@ flask app module to interact with the authentication database.
 """
 
 
-from flask import Flask, jsonify
+import email
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/")
 def index():
     """Index route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Register a user in the database."""
+    email = request.form["email"]
+    password = request.form["password"]
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as err:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
