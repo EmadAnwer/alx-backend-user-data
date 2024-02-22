7a26c96e3095d347165fb840609e2650b0e25e90
EmadAnwer <emadanwer.official@gmail.com>
2024-02-23 01:02:32 +0200

    task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,5 +71,15 @@ def profile():
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """Get a reset password token"""
+    email = request.form["email"]
+    token = AUTH.get_reset_password_token(email)
+    if token:
+        return jsonify({"email": email, "reset_token": token}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
