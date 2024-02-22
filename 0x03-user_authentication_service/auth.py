646f16084daf5dc3f9d40a9d5239f16df83c6b83
EmadAnwer <emadanwer.official@gmail.com>
2024-02-22 23:39:13 +0200

    [task 10]

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,3 +46,13 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a new session for the user"""
+        try:
+            session_id = _generate_uuid()
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
