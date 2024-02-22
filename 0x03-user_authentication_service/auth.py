b2c0cfed5c29880f6dd3516a6ffdb545d7292394
EmadAnwer <emadanwer.official@gmail.com>
2024-02-22 23:31:58 +0200

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,11 +8,17 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
     """Returns a salted hash of the input password."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Returns a string representation of a new UUID."""
+    return str(uuid.uuid4())
 
 
 class Auth:
