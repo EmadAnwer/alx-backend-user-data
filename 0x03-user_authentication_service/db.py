93f4c3ad0200207759fb7a721b2f1760733abb44
EmadAnwer <emadanwer.official@gmail.com>
2024-02-22 21:43:19 +0200

    [task 1]

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,7 +2,6 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
@@ -23,7 +22,7 @@ class DB:
     def _session(self) -> Session:
         """Memoized session object"""
         if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine, expire_on_commit=False)
+            DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
