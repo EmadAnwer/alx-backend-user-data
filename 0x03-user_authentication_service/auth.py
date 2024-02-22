14b43e40abaf4c7f7f159916bc561044e61de3e2
EmadAnwer <emadanwer.official@gmail.com>
2024-02-23 01:01:17 +0200

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,7 +42,8 @@ class Auth:
         """Validate a user's credentials."""
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+            return bcrypt.checkpw(password.encode("utf-8"),
+                                  user.hashed_password)
         except NoResultFound:
             return False
 
